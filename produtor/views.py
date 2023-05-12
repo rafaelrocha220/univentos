7048cef21ba708a196eb39fd5a48588c04e0bf53
Rafael Rocha <rafaelcruzrocha30@outlook.com>
2023-05-12 16:17:49 -0300

    atualizacao 12.05

--- produtor/views.py
+++ produtor/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-   return render(request, 'home.html', {})
+    return render(request, 'home.html', {})
 
 # Login requisition get
+
+
 def login(request):
-   return render(request, 'login.html', {})
+    return render(request, 'login.html', {})
 
 # Register requistion get
+
+
 def register(request):
-   return render(request, 'register.html', {})
+    return render(request, 'register.html', {})
+
 
 def eventos_cadastrar(request):
-   return render(request, 'evento_cadastrar.html', {})
+
+    return render(request, 'evento_cadastrar.html', {})
